4ba5a27fa29ab21570fe6e43352ecb80695184d9
MedecinKahani <theodesaintjores@gmail.com>
2026-06-26 10:56:20 +0300

    Routes /medecin -> /vueglobale dans app/page.js

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
   const router = useRouter();
   useEffect(() => {
     const s = sessionStorage.getItem('pds_user');
-    if (s) router.push('/medecin');
+    if (s) router.push('/vueglobale');
     else router.push('/login');
   }, []);
   return null;
