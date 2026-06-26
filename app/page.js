0fb01dae4eed77740366df20fd18ba0767a99e8a
MedecinKahani <theodesaintjores@gmail.com>
2026-06-26 10:54:57 +0300

    Root redirige vers medecin ou login

--- app/page.js
+++ app/page.js
@@ -5,15 +5,9 @@ import { useRouter } from 'next/navigation';
 export default function Home() {
   const router = useRouter();
   useEffect(() => {
-    const session = sessionStorage.getItem('pds_user');
-    if (session) {
-      const user = JSON.parse(session);
-      if (user.role === 'as') router.push('/as');
-      else if (user.role === 'ide') router.push('/ide');
-      else if (user.role === 'medecin') router.push('/medecin');
-    } else {
-      router.push('/login');
-    }
+    const s = sessionStorage.getItem('pds_user');
+    if (s) router.push('/medecin');
+    else router.push('/login');
   }, []);
   return null;
 }
